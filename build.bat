011c8d2cefa8dda976f2400524ec8d7bf27ef24a
ecourse <smkang@codenuri.co.kr>
2021-12-15 12:35:55 +0900

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,2 +1,23 @@
-g++ %1 -std=c++1z
-cl %1 /nologo /EHsc /Za /MD /std:c++latest
+cls
+
+@echo [101mg++ %* -std=c++23 -o gout.exe [0m
+@g++ %* -std=c++23 -o gout.exe
+
+@IF %ERRORLEVEL% EQU 0 (
+	echo [37;4m[ g++ compiling success !! run gout.exe ][0m
+  	gout.exe
+) ELSE (
+	echo [37;4m[ Error !!, g++ fail to compile. ][0m
+)
+
+@echo.
+
+@echo [101mcl %* /permissive- /std:c++latest /utf-8 /nologo /EHsc /link /out:vout.exe [0m
+@cl  %* /permissive- /std:c++latest /utf-8 /nologo /EHsc /link /out:vout.exe
+
+@IF %ERRORLEVEL%  EQU 0 (
+	echo [37;4m[ cl compiling success !! run vout.exe ][0m
+	vout.exe
+)ELSE (
+	echo [37;4m[ Error !!, cl fail to compile. ][0m
+)
